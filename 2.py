7ebc19d1c1ac09b252ae3d47543524bc793fe869
“ms1241721” <mohitsharma1241721@gmail.com>
2021-11-05 16:24:46 +0530

    some random error

--- 2.py
+++ 2.py
@@ -1,18 +1,26 @@
 from random import randint
 import time
-from flask import Flask,render_template
+from flask import Flask,render_template , request
 app = Flask(__name__)
 
-@app.route('/main')
+@app.route('/main',methods=["GET","POST"])
 def homepage():
-    x = randint(0,99)
-    y = randint(0,99)
-    return render_template('calculate.html.jinja',x=x,y=y)
+    if request.method=='GET':
+        x = randint(10,99)
+        y = randint(10,99)
+        result = None
+        # if int(request.form["x"])+int(request.form["y"]==int(request.form["result"])):
+            # result = "Correct"
+        # else:
+            # result = "Incorrect"
+        return render_template('calculate.html.jinja',x=x,y=y)
+    else:
+        return render_template('calculate.html.jinja',x=x,y=y,result=x+y)
 
 def getOutput():
     start_time = time.time()
-    x = randint(0,100)
-    y  = randint(0,100)
+    x = randint(10,100)
+    y  = randint(10,100)
     time_taken = int(time.time() - start_time)
     output = x+y
     return x,y,output,time_taken
