60a0bd2e058bba06c65808d99ad394a37b361ebd
“ms1241721” <mohitsharma1241721@gmail.com>
2021-11-05 16:37:02 +0530

    random variable removed out from requests

--- 2.py
+++ 2.py
@@ -5,18 +5,12 @@ app = Flask(__name__)
 
 @app.route('/main',methods=["GET","POST"])
 def homepage():
+    x = randint(10,99)
+    y = randint(10,99)
     if request.method=='GET':
-        x = randint(10,99)
-        y = randint(10,99)
-        result = None
-        # if int(request.form["x"])+int(request.form["y"]==int(request.form["result"])):
-            # result = "Correct"
-        # else:
-            # result = "Incorrect"
         return render_template('calculate.html.jinja',x=x,y=y)
     else:
-        return render_template('calculate.html.jinja',x=x,y=y,result=x+y)
-
+        return render_template('calculate.html.jinja',x=x,y=y)
 def getOutput():
     start_time = time.time()
     x = randint(10,100)
